6e804fa3602f00bc81924ce75e920941ecc676f7
Amelia-Mowers <105567018+Amelia-Mowers@users.noreply.github.com>
2024-10-17 15:12:36 -0700

    Update reader_async.rs

--- src/reader_async.rs
+++ src/reader_async.rs
@@ -1,7 +1,6 @@
 use std::{future::Future, path::Path};
 
 use tokio::{
-    fs::File,
     io::{AsyncBufRead, BufReader},
 };
 
@@ -24,45 +23,4 @@ pub trait AsyncResourceReader {
         &mut self,
         path: &Path,
     ) -> impl Future<Output = Result<Self::Resource, Self::Error>>;
-}
-
-/// An [`AsyncResourceReader`] that reads from Tokio [`File`] handles.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
-pub struct AsyncFilesystemResourceReader;
-
-impl AsyncFilesystemResourceReader {
-    /// Creates a new [`AsyncFilesystemResourceReader`].
-    pub fn new() -> Self {
-        Self
-    }
-}
-
-impl AsyncResourceReader for AsyncFilesystemResourceReader {
-    type Resource = BufReader<File>;
-    type Error = std::io::Error;
-
-    async fn read_from(&mut self, path: &Path) -> std::result::Result<Self::Resource, Self::Error> {
-        let file = File::open(path).await?;
-        Ok(BufReader::new(file))
-    }
-}
-
-impl<T, F, R, E> AsyncResourceReader for T
-where
-    T: for<'a> Fn(&'a Path) -> F,
-    F: Future<Output = Result<R, E>>,
-    R: AsyncBufRead + Unpin,
-    E: std::error::Error + Send + Sync + 'static,
-{
-    type Resource = R;
-
-    type Error = E;
-
-    // FIXME: remove `allow(refining_impl_trait)` or this comment or add #[refine]
-    // this lint is under discussion at https://github.com/rust-lang/rust/issues/121718
-    // eventually, we'd want to #[refine] this function if this attribute becomes available
-    #[allow(refining_impl_trait)]
-    fn read_from(&mut self, path: &Path) -> F {
-        self(path)
-    }
 }
